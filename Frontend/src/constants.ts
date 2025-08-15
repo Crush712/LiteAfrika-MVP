76dd7390193450cb6f9eb660167d23c39f5921e6
johnmartinshillary <ekwealorhillary@gmail.com>
2025-08-15 16:57:01 +0100

    .

--- Frontend/src/constants.ts
+++ Frontend/src/constants.ts
@@ -2,4 +2,4 @@ export const PACKAGE_ID =
   "0x5c7d2c8ff6c4aac80509ee49e9498d6c6aae73061884aca7a65df70dffad78a7";
 export const MODULE_NAME = "HubRegistry";
 export const REGISTRY_ID =
-  "0x1ae6e2fd6d6821868fbe1c67044fbe5801a1e075c7a043db00d8e39dc645fdb8";
+  "0xac129614f617b79cedd3bda4fb408183dbf6778fed2778ee057752f72509cc60";
